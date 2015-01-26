3ccc9d1bcbe5f2a4bd74741c3b53d1c300d50482
Stephen Gehring <sgehringdesign@gmail.com>
2015-01-26 02:41:15 -0800

    add first lines of js from tut

--- apps/app.js
+++ apps/app.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
 
-  
+  $(function(){
+    'use strict'
+
+    $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+      console.log(data);
+    });
+
+  })
 });
