e1e07082e0ebff27c12fde96f9da034f1fe403ff
Stephen Gehring <sgehringdesign@gmail.com>
2015-01-26 02:44:13 -0800

    section 3 of tut.. shorter syntax

--- apps/app.js
+++ apps/app.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
   'use strict'
+  /*
   $(function(){
     $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
       console.log(data);
     })
   })
+  */
+  $.get('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+    console.log(data);
+  }, 'json')
 });
